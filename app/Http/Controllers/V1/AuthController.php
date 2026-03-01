9320505ab8520d8f9309c1913c61bd3cc0120d08
Toni <tonirachev92@gmail.com>
2026-03-01 14:23:31 +0200

    feat: add login method

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -4,16 +4,16 @@ namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Auth\RegisterUserRequest;
+use App\Http\Requests\V1\LoginAuthRequest;
 use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use App\Responses\V1\ApiResponse;
 use App\Services\V1\AuthService;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function __construct(private AuthService $authService)
+    public function __construct(private readonly AuthService $authService)
     {
     }
 
@@ -33,6 +33,19 @@ class AuthController extends Controller
         $result = $this->authService->register($request->validated());
         return ApiResponse::created(
             'User created successfully',
+            [
+                'user' => new UserResource($result['user']),
+                'token' => $result['token'],
+            ]
+        );
+    }
+
+    public function login(LoginAuthRequest $request)
+    {
+        $result = $this->authService->login($request->validated());
+
+        return ApiResponse::ok(
+            'User logged in successfully',
             [
                 'user' => new UserResource($result['user']),
                 'token' => $result['token'],
