8398f5f35efebd652916aee7ced5a71d3f466b6d
Toni <tonirachev92@gmail.com>
2026-03-03 17:24:17 +0200

    refactor: replaced magic route name

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,5 +4,5 @@ use App\Constants\Routes;
 use App\Http\Controllers\V1\AuthController;
 
 Route::prefix('auth')->name('auth.')->group(function () {
-    Route::post(Routes::REGISTER, [AuthController::class, 'register'])->name('register');
+    Route::post(Routes::REGISTER, [AuthController::class, 'register'])->name(Routes::REGISTER);
 });
