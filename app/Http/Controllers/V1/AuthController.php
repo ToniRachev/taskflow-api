6b204648fc8d5832926bd868350647bd42fda165
Toni <tonirachev92@gmail.com>
2026-03-03 12:25:20 +0200

    feat: basic user registration

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\V1;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\V1\Auth\RegisterAuthRequest;
+use App\Http\Resources\V1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(RegisterAuthRequest $request)
     {
-        return User::create($request->all());
+        $user = User::create($request->validated());
+
+        return new UserResource($user);
     }
 }
