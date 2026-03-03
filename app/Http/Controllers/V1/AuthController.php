c4698a2bcf7c5f9e0ade17367975befb7bf88c37
Toni <tonirachev92@gmail.com>
2026-03-03 12:35:40 +0200

    refactor: register user with API response and constants

--- app/Http/Controllers/V1/AuthController.php
+++ app/Http/Controllers/V1/AuthController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Auth\RegisterAuthRequest;
 use App\Http\Resources\V1\UserResource;
+use App\Http\Responses\V1\ApiResponse;
 use App\Models\User;
-use Illuminate\Http\Request;
+use App\Constants\Message;
 
 class AuthController extends Controller
 {
     public function register(RegisterAuthRequest $request)
     {
-        $user = User::create($request->validated());
-
-        return new UserResource($user);
+        return ApiResponse::created(
+            Message::USER_REGISTERED,
+            new UserResource(
+                User::create($request->validated())
+            )
+        );
     }
 }
