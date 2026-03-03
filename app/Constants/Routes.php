aaae6b90877989f442ed3e0b8c5bb328c126e66b
Toni <tonirachev92@gmail.com>
2026-03-03 21:14:15 +0200

    feat: add login route

--- app/Constants/Routes.php
+++ app/Constants/Routes.php
@@ -4,7 +4,8 @@ namespace App\Constants;
 
 class Routes
 {
-    public const API_VERSION = 'api.v1';
-    public const AUTH_MODULE = 'auth';
+    public const string API_VERSION = 'api.v1';
+    public const string AUTH_MODULE = 'auth';
     public const string REGISTER = 'register';
+    public const string LOGIN = 'login';
 }
