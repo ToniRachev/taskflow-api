2d35347fd54ebbe6aea833a859113f3147bff9ad
Toni <tonirachev92@gmail.com>
2026-03-03 17:13:40 +0200

    feat: add register tests

---
+++ tests/Feature/RegisterTest.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+
+describe('Register', function () {
+    beforeEach(function () {
+        $this->payload = [
+            'name' => 'Test',
+            'email' => 'test@gmail.com',
+            'password' => 'testPassword123$',
+        ];
+    });
+
+    it('should register a user and returns the correct structure', function () {
+        $response = $this->postJson('/api/v1/auth/register', $this->payload)
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'success',
+                'message',
+                'data' => ['user', 'token']
+            ]);
+
+        $this->assertDatabaseHas('users', ['email' => $this->payload['email']]);
+        expect($response->json('data.token'))->toBeString()->not->toBeEmpty();
+    });
+
+    it('fail if the email is already registered', function () {
+        \App\Models\User::factory()->create([
+            'email' => 'test@gmail.com'
+        ]);
+
+        $this->postJson('/api/v1/auth/register', $this->payload)
+            ->assertStatus(422)
+            ->assertJsonValidationErrors(['email']);
+    });
+
+    it('fails validation on missing data', function ($invalidData, $field) {
+        $this->postJson('/api/v1/auth/register', array_merge($this->payload, $invalidData))
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([$field]);
+    })->with([
+        'missing name' => [['name' => ''], 'name'],
+        'missing email' => [['email' => ''], 'email'],
+        'missing password' => [['password' => ''], 'password'],
+    ]);
+
+    it('fail validation on incorrect data', function ($invalidData, $field) {
+        $this->postJson('/api/v1/auth/register', array_merge($this->payload, $invalidData))
+            ->assertStatus(422)
+            ->assertJsonValidationErrors([$field]);
+    })->with([
+        'invalid email' => [['email' => 'test'], 'email'],
+        'invalid password' => [['password' => 'test'], 'password'],
+        'long name' => [['name' => str_repeat('a', 256)], 'name'],
+    ]);
+
+    it('hash password', function () {
+        $response = $this->postJson('/api/v1/auth/register', $this->payload);
+
+        $user = \App\Models\User::where('email', $this->payload['email'])->first();
+        $this->assertNotEquals($this->payload['password'], $user->password);
+    });
+});
