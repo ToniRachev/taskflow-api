78a2be5a37cd322a21778bf09afa5114b25e3093
Toni <tonirachev92@gmail.com>
2026-03-03 13:22:59 +0200

    feat: add error messages

--- app/Constants/Message.php
+++ app/Constants/Message.php
@@ -4,5 +4,8 @@ namespace App\Constants;
 
 class Message
 {
+    public const string RESOURCE_CREATED = 'Resource created successfully';
     public const string USER_REGISTERED = 'User registered successfully';
+    public const string VALIDATION_ERROR = 'Validation error';
+    public const string SERVER_ERROR = 'Something went wrong. We are doing our best to fix it.';
 }
