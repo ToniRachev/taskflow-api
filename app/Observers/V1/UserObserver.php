4126b065163a8f8e3f145344a54514d46c16d76b
Toni <tonirachev92@gmail.com>
2026-03-04 20:30:43 +0200

    feat: use defaultPreferences for profile creation in UserObserver

--- app/Observers/V1/UserObserver.php
+++ app/Observers/V1/UserObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers\V1;
 
+use App\Models\V1\Profile;
 use App\Models\V1\User;
 
 class UserObserver
@@ -11,7 +12,7 @@ class UserObserver
      */
     public function created(User $user): void
     {
-        $user->profile()->create();
+        $user->profile()->create(Profile::defaultPreferences());
     }
 
     /**
