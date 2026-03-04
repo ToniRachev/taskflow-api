178d552b7f46d13f931aa9b57fd6f4d59cdc27bc
Toni <tonirachev92@gmail.com>
2026-03-04 20:30:29 +0200

    feat: add profile routes for fetching and updating user profile and preferences

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use App\Constants\Routes;
 use App\Http\Controllers\V1\AuthController;
+use App\Http\Controllers\V1\ProfileController;
 
 Route::prefix(Routes::AUTH_MODULE)->name(Routes::AUTH_MODULE . '.')->group(function () {
     Route::post(Routes::REGISTER, [AuthController::class, 'register'])->name(Routes::REGISTER);
@@ -12,4 +13,10 @@ Route::prefix(Routes::AUTH_MODULE)->name(Routes::AUTH_MODULE . '.')->group(funct
         Route::post(Routes::LOGOUT_ALL, [AuthController::class, 'logoutAll'])->name(Routes::LOGOUT_ALL);
         Route::post(Routes::REFRESH_TOKEN, [AuthController::class, 'refreshToken'])->name(Routes::REFRESH_TOKEN);
     });
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('me', [ProfileController::class, 'show']);
+    Route::patch('profile', [ProfileController::class, 'update']);
+    Route::patch('profile/preferences', [ProfileController::class, 'updatePreferences']);
 });
