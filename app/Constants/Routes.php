7b6506a0786f99c232c5ab30d587200ec7b653e0
Toni <tonirachev92@gmail.com>
2026-03-04 20:31:47 +0200

    refactor: remove redundant `string` type declarations in constants files

--- app/Constants/Routes.php
+++ app/Constants/Routes.php
@@ -4,11 +4,11 @@ namespace App\Constants;
 
 class Routes
 {
-    public const string API_VERSION = 'api.v1';
-    public const string AUTH_MODULE = 'auth';
-    public const string LOGIN = 'login';
-    public const string REGISTER = 'register';
-    public const string LOGOUT = 'logout';
-    public const string LOGOUT_ALL = 'logout-all';
-    public const string REFRESH_TOKEN = 'refresh-token';
+    public const API_VERSION = 'api.v1';
+    public const AUTH_MODULE = 'auth';
+    public const LOGIN = 'login';
+    public const REGISTER = 'register';
+    public const LOGOUT = 'logout';
+    public const LOGOUT_ALL = 'logout-all';
+    public const REFRESH_TOKEN = 'refresh-token';
 }
