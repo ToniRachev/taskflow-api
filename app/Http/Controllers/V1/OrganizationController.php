2c8e68338b054ad9eb170d0abcf668d18098551b
Toni <tonirachev92@gmail.com>
2026-03-07 12:59:17 +0200

    feat: expand OrganizationController with show, update, index, and members endpoints for improved functionality

--- app/Http/Controllers/V1/OrganizationController.php
+++ app/Http/Controllers/V1/OrganizationController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\V1;
 
 use App\Constants\Message;
-use App\Enums\OrganizationMembershipRoleEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Organization\StoreOrganizationRequest;
+use App\Http\Requests\V1\Organization\UpdateOrganizationRequest;
 use App\Http\Resources\V1\MemberResource;
-use App\Http\Resources\V1\OrganizationResource;
+use App\Http\Resources\V1\Organization\OrganizationDetailResource;
+use App\Http\Resources\V1\Organization\OrganizationResource;
 use App\Http\Responses\V1\ApiResponse;
 use App\Models\V1\Organization;
 use App\Services\V1\OrganizationService;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 
 class OrganizationController extends Controller
 {
@@ -30,11 +30,34 @@ class OrganizationController extends Controller
                 $request->user()->id,
                 $request->validated('logo')
             );
-        return ApiResponse::created(Message::ORGANIZATION_CREATED, new OrganizationResource($organization));
+        return ApiResponse::created(Message::ORGANIZATION_CREATED, OrganizationResource::make($organization));
     }
 
-    public function index(Organization $organization)
+    public function members(Organization $organization): JsonResponse
     {
         return ApiResponse::ok(data: MemberResource::collection($organization->members));
+    }
+
+    public function index(Request $request): JsonResponse
+    {
+        return ApiResponse::ok(data: OrganizationResource::collection($request->user()->organizations));
+    }
+
+    public function show(Organization $organization): JsonResponse
+    {
+        $this->authorize('view', $organization);
+        return ApiResponse::ok(data: OrganizationDetailResource::make($organization->loadCount('members')));
+    }
+
+    public function update(UpdateOrganizationRequest $request, Organization $organization): JsonResponse
+    {
+        $updatedOrganization = $this->organizationService
+            ->updateOrganization(
+                $organization,
+                $request->validated(),
+                $request->hasFile('logo')
+            );
+
+        return ApiResponse::ok(data: OrganizationResource::make($updatedOrganization));
     }
 }
