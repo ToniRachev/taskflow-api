ec9b6e54671d73c9bddf6a09115d96a452912fbe
Toni <tonirachev92@gmail.com>
2026-03-08 19:15:27 +0200

    feat: add StoreProjectRequest with validation rules for project creation

---
+++ app/Http/Requests/V1/Project/StoreProjectRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests\V1\Project;
+
+use App\Enums\V1\ProjectVisibilityEnum;
+use App\Models\V1\Project;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreProjectRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'key' => [
+                'sometimes',
+                'string',
+                'max:10',
+                'alpha_num',
+                Rule::unique('projects', 'key')
+                    ->where('organization_id', $this->route('organization')->id)
+            ],
+            'description' => ['nullable', 'string'],
+            'visibility' => ['sometimes', Rule::enum(ProjectVisibilityEnum::class)],
+            'start_date' => ['nullable', 'date'],
+            'end_date' => ['nullable', 'date', 'after:start_date']
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'key.unique' => 'Project key already exists'
+        ];
+    }
+}
