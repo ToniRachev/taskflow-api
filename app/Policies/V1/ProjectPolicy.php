cbc1f4b8ee91af68fdca6373485dd1ba8dc85f09
Toni <tonirachev92@gmail.com>
2026-03-08 19:15:09 +0200

    feat: add ProjectPolicy with permission checks for CRUD and archive operations

---
+++ app/Policies/V1/ProjectPolicy.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Policies\V1;
+
+use App\Enums\OrganizationMembershipRoleEnum;
+use App\Models\V1\Organization;
+use App\Models\V1\Project;
+use App\Models\V1\User;
+use Illuminate\Auth\Access\Response;
+
+class ProjectPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user, Organization $organization): bool
+    {
+        return $organization->isMember($user);
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Project $project): bool
+    {
+        return $project->organization->isMember($user);
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, Organization $organization): bool
+    {
+        return $organization->hasAdminAccess($user);
+    }
+
+    /**p
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Project $project): bool
+    {
+        return $project->organization->hasAdminAccess($user);
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Project $project): bool
+    {
+        return $project->organization->hasAdminAccess($user);
+    }
+
+    public function archive(User $user, Project $project) {
+        return $project->organization->hasAdminAccess($user);
+    }
+}
