712caa9141c491f0cb4417f63bc5982b51e323cb
Toni <tonirachev92@gmail.com>
2026-03-17 00:16:00 +0200

    refactor: remove unused ProjectCreatedResource and ProjectListResource imports in ProjectController

--- app/Http/Controllers/V1/ProjectController.php
+++ app/Http/Controllers/V1/ProjectController.php
@@ -8,8 +8,6 @@ use App\Enums\V1\TaskStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Project\StoreProjectRequest;
 use App\Http\Requests\V1\Project\UpdateProjectRequest;
-use App\Http\Resources\V1\Project\ProjectCreatedResource;
-use App\Http\Resources\V1\Project\ProjectListResource;
 use App\Http\Resources\V1\Project\ProjectResource;
 use App\Http\Responses\V1\ApiResponse;
 use App\Models\V1\Organization;
