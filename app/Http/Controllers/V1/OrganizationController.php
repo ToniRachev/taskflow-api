e12e02e0c72a30553fd31f51d1eadb371b599877
Toni <tonirachev92@gmail.com>
2026-03-17 00:32:47 +0200

    refactor: remove unused OrganizationDetailResource import from OrganizationController

--- app/Http/Controllers/V1/OrganizationController.php
+++ app/Http/Controllers/V1/OrganizationController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\Organization\StoreOrganizationRequest;
 use App\Http\Requests\V1\Organization\UpdateOrganizationRequest;
 use App\Http\Resources\V1\MemberResource;
-use App\Http\Resources\V1\Organization\OrganizationDetailResource;
 use App\Http\Resources\V1\OrganizationResource;
 use App\Http\Responses\V1\ApiResponse;
 use App\Models\V1\Organization;
