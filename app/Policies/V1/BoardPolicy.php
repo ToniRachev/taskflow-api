29b1d53549d75e2199daa706cda503c3360c29a1
Toni <tonirachev92@gmail.com>
2026-03-18 11:01:47 +0200

    feat: add BoardPolicy to handle authorization logic for Board actions

---
+++ app/Policies/V1/BoardPolicy.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Policies\V1;
+
+use App\Models\V1\Board;
+use App\Models\V1\Project;
+use App\Models\V1\User;
+use Illuminate\Auth\Access\Response;
+
+class BoardPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Board $board): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user, Project $project): bool
+    {
+        return $project->organization->hasAdminAccess($user);
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Board $board): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Board $board): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Board $board): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Board $board): bool
+    {
+        return false;
+    }
+}
