a5a41e9a8ab1a018da3e7a3e711fa698c5dedfec
Toni <tonirachev92@gmail.com>
2026-03-18 13:38:35 +0200

    feat: add board listing and creation routes under projects

--- routes/groups/projects.php
+++ routes/groups/projects.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Constants\Routes;
+use App\Http\Controllers\V1\BoardController;
 use App\Http\Controllers\V1\ProjectController;
 use App\Http\Controllers\V1\TaskController;
 
@@ -11,11 +12,14 @@ Route::prefix('/{project}')->group(function () {
     Route::delete('/', [ProjectController::class, 'destroy'])->name(Routes::PROJECT_DESTROY);
     Route::post('/archive', [ProjectController::class, 'archiveProject'])->name(Routes::PROJECT_ARCHIVE);
 
-    Route::prefix('/boards')->group(base_path('routes/groups/boards.php'));
-
     Route::prefix('tasks')->group(function () {
         Route::get('/', [TaskController::class, 'index'])->name(Routes::TASK_INDEX);
         Route::post('/', [TaskController::class, 'store'])->name(Routes::TASK_STORE);
         Route::patch('/bulk-status', [TaskController::class, 'updateBulkStatus'])->name(Routes::TASK_STATUS_BULK);
+    });
+
+    Route::prefix('/boards')->controller(BoardController::class)->group(function () {
+        Route::get('/', 'index')->name(Routes::BOARD_INDEX);
+        Route::post('/', 'store')->name(Routes::BOARD_STORE);
     });
 });
