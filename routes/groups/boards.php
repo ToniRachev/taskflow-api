9ae390aca11f507fd9e1455d15bf6522141ab0b5
Toni <tonirachev92@gmail.com>
2026-03-18 13:38:27 +0200

    refactor: update board routes to replace index and store with show route

--- routes/groups/boards.php
+++ routes/groups/boards.php
@@ -4,6 +4,5 @@ use App\Constants\Routes;
 use App\Http\Controllers\V1\BoardController;
 
 Route::controller(BoardController::class)->group(function () {
-    Route::get('/', 'index')->name(Routes::BOARD_INDEX);
-    Route::post('/', 'store')->name(Routes::BOARD_STORE);
+    Route::get('/{board}', 'show')->name(Routes::BOARD_SHOW);
 });
